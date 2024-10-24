55bf280f992a91f4f9a1acca6319dcc208880963
Yehor Pliasov <fenix@yehors-macbook-air.home>
2024-10-24 12:17:44 +0200

    upgrade dotnet and packages version into 6

--- BookShop.Web/Program.cs
+++ BookShop.Web/Program.cs
@@ -1,20 +1,47 @@
-using Microsoft.AspNetCore.Hosting;
+using BookShop.Infrastructure.Context;
+using BookShop.Infrastructure.Handlers.Commands;
+using BookShop.Infrastructure.Handlers.Queries;
+using BookShop.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace BookShop.Web
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+builder.Services.AddDbContext<ShopDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddTransient<AuthorRepository>();
+builder.Services.AddTransient<CreateAuthorCommandHandler>();
+builder.Services.AddTransient<GetAuthorListQueryHandler>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+
+app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Authors}/{action=AuthorList}/{id?}");
+
+app.Run();        
