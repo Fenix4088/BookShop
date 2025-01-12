272b116ae26e5ef0f331f04f2eba9a6c2f7deec0
fenix <ypliasov@openskydata.com>
2025-01-12 14:39:41 +0100

    UseInfrastructure middleware

--- BookShop.Infrastructure/Extensions.cs
+++ BookShop.Infrastructure/Extensions.cs
@@ -1,7 +1,12 @@
 using BookShop.Infrastructure.Context;
+using BookShop.Infrastructure.Handlers.Commands;
+using BookShop.Infrastructure.Handlers.Queries;
+using BookShop.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace BookShop.Infrastructure;
 
@@ -10,10 +15,38 @@ public static class Extensions
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<ShopDbContext>(options =>
-            options.UseSqlServer(
-                configuration.GetConnectionString("BookShop")));
-        services.AddDatabaseDeveloperPageExceptionFilter();
+                options.UseSqlServer(
+                    configuration.GetConnectionString("BookShop")))
+            .AddDatabaseDeveloperPageExceptionFilter()
+            .AddTransient<AuthorRepository>()
+            .AddTransient<CreateAuthorCommandHandler>()
+            .AddTransient<GetAuthorListQueryHandler>();
 
         return services;
+    }
+
+    public static WebApplication UseInfrastructure(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Authors}/{action=AuthorList}/{id?}");
+        return app;
     }
 }
--- BookShop.Web/Program.cs
+++ BookShop.Web/Program.cs
@@ -1,53 +1,16 @@
 using BookShop.Domain.Validators;
 using BookShop.Infrastructure;
-using BookShop.Infrastructure.Context;
-using BookShop.Infrastructure.Handlers.Commands;
-using BookShop.Infrastructure.Handlers.Queries;
-using BookShop.Infrastructure.Repositories;
 using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddControllersWithViews().AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<CreateAuthorValidation>());
-
-// builder.Services.AddDbContext<ShopDbContext>(options =>
-//     options.UseSqlServer(
-//         builder.Configuration.GetConnectionString("BookShop")));
-// builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-builder.Services.AddInfrastructure(builder.Configuration);
-
-
-builder.Services.AddTransient<AuthorRepository>();
-builder.Services.AddTransient<CreateAuthorCommandHandler>();
-builder.Services.AddTransient<GetAuthorListQueryHandler>();
+builder.Services
+    .AddInfrastructure(builder.Configuration)
+    .AddControllersWithViews()
+    .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<CreateAuthorValidation>());
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-
-app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Authors}/{action=AuthorList}/{id?}");
-
+app.UseInfrastructure();
 app.Run();        
